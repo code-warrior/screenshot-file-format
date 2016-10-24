179da91a6048a85bc93632a5f88913114d408874
Roy A Vanegas <roy@thecodeeducators.com>
2016-10-24 18:35:04 -0400

    Indent text replacement for the constant `RESPONSE_LENGTH`

--- constants.h
+++ constants.h
@@ -24,8 +24,8 @@
 #define BLACK            "\033[0m"
 #define BOLDRED          "\033[31m\033[1m"
 
-#define RESPONSE_LENGTH  32
 #define MAX_INPUT_LENGTH 12
 #define INVALID_INPUT    -1
+#define RESPONSE_LENGTH        32
 
 #endif
