b40cfcf4f059b01ae4b90575265497150689f4bc
Roy Vanegas <roy@thecodeeducators.com>
2016-10-27 20:53:31 -0400

    Add BLUE to constants.h

--- constants.h
+++ constants.h
@@ -24,6 +24,7 @@
 #define NORMAL                   "\033[0m"
 #define RED                      "\033[31m"
 #define GREEN                    "\033[32m"
+#define BLUE                     "\033[34m"
 
 #define RESPONSE_LENGTH          32
 #define MAX_INPUT_LENGTH         12
