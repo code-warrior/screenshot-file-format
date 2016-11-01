be6eee470619c5af91e5f857c4a7ab3cb2a81290
Roy A Vanegas <roy@thecodeeducators.com>
2016-10-31 20:24:55 -0400

    Refactor the JPG case so it won’t set JPG if it is already set

--- main.c
+++ main.c
@@ -212,13 +212,20 @@ int main (void)
 
          case 'J':
          case 'j':
-            if (NULL == (command_response = popen("defaults write com.apple.screencapture type jpg", "r"))) {
-               printf("Unable to set JPG. Either memory could not be allocated, or forking or piping failed. Please report this issue on GitHub at https://github.com/code-warrior/screenshot-file-format/issues or via email at roy@thecodeeducators.com.\n");
+            if (0 == strcmp(current_file_format, "JPG")) {
+               printf("\t%sJPG%s is the current file format. No change has been made to your system. Exiting…\n\n", BLUE, NORMAL);
 
-               exit(EXIT_FAILURE);
+               exit(EXIT_SUCCESS);
 
             } else {
-               printf("\t%sJPG%s was set.\n\n", BLUE, NORMAL);
+               if (NULL == (command_response = popen("defaults write com.apple.screencapture type jpg", "r"))) {
+                  printf("Unable to set JPG. Either memory could not be allocated, or forking or piping failed. Please report this issue on GitHub at https://github.com/code-warrior/screenshot-file-format/issues or via email at roy@thecodeeducators.com.\n");
+
+                  exit(EXIT_FAILURE);
+
+               } else {
+                  printf("\t%sJPG%s was set.\n\n", BLUE, NORMAL);
+               }
             }
 
             break;
